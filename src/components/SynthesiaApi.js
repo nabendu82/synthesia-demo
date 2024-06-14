cf82c9c147fd59655d43936d01236857b70fca28
nabendu82 <nabendu.biswas@gmail.com>
2024-06-14 12:46:31 +0530

    GET video done

--- src/components/SynthesiaApi.js
+++ src/components/SynthesiaApi.js
@@ -1,16 +1,48 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 const SynthesiaApi = () => {
     const [title, setTitle] = useState('')
     const [desc, setDesc] = useState('')
     const [script, setScript] = useState('')
+    const [videoId, setVideoId] = useState('')
+    const [downloadUrl, setDownloadUrl] = useState('')
+
+    useEffect(() => {
+        console.log("useEffect running videoId ", videoId)
+        if (videoId) {
+            const intervalId = setInterval(async () => {
+                const videoInfo = await getVideo(videoId);
+                console.log(videoInfo);
+                if (videoInfo.status === 'complete') {
+                    setDownloadUrl(videoInfo.download);
+                    clearInterval(intervalId);
+                }
+            }, 15000);
+
+            return () => clearInterval(intervalId);
+        }
+    }, [videoId]);
+
+    const getVideo = async(videoId) => {
+        try {
+            const response = await axios.get(`https://api.synthesia.io/v2/videos/${videoId}`, {
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': `${process.env.REACT_APP_API_KEY}`
+                }
+            })
+            return response.data
+        } catch (error) {
+            console.log('error in getVideo ', error)
+        }
+    }
 
     const submitForm = async e => {
         e.preventDefault()
         console.log({ title, desc, script })
         const data = {
-            test: 'true',
+            test: 'false',
             visibility: 'private',
             title: title,
             description: desc,
@@ -44,6 +76,8 @@ const SynthesiaApi = () => {
         axios(config)
             .then(response => {
                 console.log(response.data);
+                console.log(response.data.id);
+                setVideoId(response.data.id);
             })
             .catch(error => {
                 console.error(error);
@@ -54,15 +88,23 @@ const SynthesiaApi = () => {
     }
 
     return (
-        <form onSubmit={submitForm}>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" value={title} onChange={e => setTitle(e.target.value)} />
-            <label htmlFor="description">Description</label>
-            <input type="text" id="description" value={desc} onChange={e => setDesc(e.target.value)} />
-            <label htmlFor="script">Script</label>
-            <input type="text" id="script" value={script} onChange={e => setScript(e.target.value)} />
-            <button type="submit">Submit</button>
-        </form>
+        <>
+            <form onSubmit={submitForm}>
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" value={title} onChange={e => setTitle(e.target.value)} />
+                <label htmlFor="description">Description</label>
+                <input type="text" id="description" value={desc} onChange={e => setDesc(e.target.value)} />
+                <label htmlFor="script">Script</label>
+                <input type="text" id="script" value={script} onChange={e => setScript(e.target.value)} />
+                <button type="submit">Submit</button>
+            </form>
+            {downloadUrl && (
+                <div>
+                    <h3>Download Your Video</h3>
+                    <a href={downloadUrl} download>Download Video</a>
+                </div>
+            )}
+        </>
     )
 }
 export default SynthesiaApi
